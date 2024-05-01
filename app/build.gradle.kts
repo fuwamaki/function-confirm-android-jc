b99e76fff9e7eebf2f5ed7ee02b17bbb1372889a
fuwamaki <variety.fuwa.3@gmail.com>
2024-05-01 10:23:20 +0900

    realize navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    // デフォルトで入ってたpackages
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,4 +67,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // 追加 ※記述だとうまく追加できない。設定(Command+;)から追加すると良い。
+    implementation(libs.androidx.navigation.compose)
 }
--- app/src/main/java/com/example/jcsample/MainActivity.kt
+++ app/src/main/java/com/example/jcsample/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,28 +22,48 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.jcsample.ui.theme.JCSampleTheme
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JCSampleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FunctionList()
+                    DisplayNav()
                 }
             }
         }
     }
 }
 
+enum class Nav {
+    TopScreen,
+    GitRepoScreen,
+}
+
 @Composable
-fun FunctionList() {
+fun DisplayNav() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Nav.TopScreen.name) {
+        composable(route = Nav.TopScreen.name) {
+            TopScreen(navController)
+        }
+        composable(route = Nav.GitRepoScreen.name) {
+            GitRepoScreen(navController)
+        }
+    }
+}
+
+@Composable
+fun TopScreen(navController: NavController) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -50,19 +72,32 @@ fun FunctionList() {
         verticalArrangement = Arrangement.spacedBy(1.dp)
     ) {
         items(50) { index ->
-            NumberListItem(number = index + 1)
+            NumberListItem(navController, number = index)
         }
     }
 }
 
 @Composable
-fun NumberListItem(number: Int) {
+fun GitRepoScreen(navController: NavController) {
+    Column {
+        Text(text = "Screen 2")
+        Button(onClick = { navController.navigate(Nav.TopScreen.name) }) {
+            Text(text = "Back to Screen 1")
+        }
+    }
+}
+
+@Composable
+fun NumberListItem(navController: NavController, number: Int) {
     val context = LocalContext.current
     Row(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Gray)
             .clickable {
+                if (number == 0) {
+                    navController.navigate(Nav.GitRepoScreen.name)
+                }
                 Toast
                     .makeText(context, "click test $number", Toast.LENGTH_SHORT)
                     .show()
@@ -80,6 +115,6 @@ fun NumberListItem(number: Int) {
 @Composable
 fun FunctionListPreview() {
     JCSampleTheme {
-        FunctionList()
+        DisplayNav()
     }
 }
